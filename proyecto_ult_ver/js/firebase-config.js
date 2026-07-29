d91ddc5ad9561de81929988004cb26ebbf1804ec
Fernando <fergama0178@gmail.com>
2026-07-29 18:33:19 -0500

    Fix: eliminar carpeta duplicada proyecto_ult_ver

--- proyecto_ult_ver/js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-const FIREBASE_CONFIG = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.firebasestorage.app",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
-};
